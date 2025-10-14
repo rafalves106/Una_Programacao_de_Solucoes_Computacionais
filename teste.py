6cd1f52f7aead0929d694e301f6b682fb375b22a
Rafael (Falves) Alves <rafalves106@icloud.com>
2025-10-14 18:19:43 -0300

    Finalizando exercícios avaliativos

--- teste.py
+++ teste.py
@@ -1,16 +1,11 @@
-somaIdades = 0
-contadorPeso = 0
+n = int(input("Digite o valor de n: "))
+a = 0
+b = 1
+c = a + b
+cotador = 0
 
-for i in range(7):
-    idade = int(input("Informe a idade de uma pessoa: "))
-    peso = float(input("Informe o peso de uma pessoa em kg: "))
-    
-    if peso > 90:
-        contadorPeso += 1
-        
-    somaIdades += idade
-    
-mediaIdades = somaIdades / 7
-    
-print("Quantidade de pessoas com mais de 90kg é", contadorPeso)
-print("A média de idades foi: %.0f" % mediaIdades, "anos.")
+while contador <= n:
+    contador += 1
+    a = b
+    b = c
+    print(a, b, c, contador)
